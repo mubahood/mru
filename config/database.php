350a539008701d83b65757dbc30914125602ba99
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2026-01-02 02:09:48 +0300

    fix: Update remote database password and add SSL options for enhanced security

--- config/database.php
+++ config/database.php
@@ -69,13 +69,17 @@ return [
             'port' => env('REMOTE_DB_PORT', '3306'),
             'database' => env('REMOTE_DB_DATABASE', 'campus_dynamics'),
             'username' => env('REMOTE_DB_USERNAME', 'dbmanager'),
-            'password' => env('REMOTE_DB_PASSWORD', ''),
+            'password' => env('REMOTE_DB_PASSWORD', '24thdecember1977'),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => false,
             'engine' => 'InnoDB',
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'pgsql' => [
